40141aead2a34af40bf98183884567ef4059f664
maks3d12 <maks-10-05@mail.ru>
2023-06-14 18:33:14 +0300

    practicIninstitution

---
+++ ProjectLib/Json.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectLib
+{
+     public class Json
+    {
+        public static void MySerialize<T>(string path, T Content)
+        {
+            if (!File.Exists(path))
+            {
+                FileStream filestream = File.Create(path);
+                filestream.Dispose();
+            }
+            string json = JsonConvert.SerializeObject(Content);
+            File.WriteAllText(path, json);
+        }
+        static public T ReadFromFile<T>(string path)
+        {
+            
+            string resultInfo = File.ReadAllText(path);
+            T result = JsonConvert.DeserializeObject<T>(resultInfo);
+           
+            return result;
+        }
+    }
+}
---
+++ TestMyLibary/ViewModel/MainViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TestMyLibary.ViewModel.Helper;
+using ProjectLib;
+using System.IO;
+
+namespace TestMyLibary.ViewModel
+{
+    internal class MainViewModel : BindingHelper
+    {
+        #region Св-ва
+        public BindableCommand AddCommand { get; set; }
+        public BindableCommand AddSecondComand { get; set; }
+        /*string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);*/ // Если прочитаете, можете коментом сказать как дать себе доступ к созданию файла на рабочем столе?
+
+        public MainViewModel()
+        {
+            AddCommand = new BindableCommand(_ => Serialze());
+            AddSecondComand = new BindableCommand(_ => ReadFromFile());
+
+        }
+        string textFile = "запуск";
+        public string TextInFile
+        {
+            get { return textFile; }
+            set { textFile = value; OnPropertyChanged(); }
+        }
+        #endregion
+
+        public void Serialze()
+        {
+
+            Json.MySerialize("json.json", textFile);
+            MessageBox.Show("Сохранили");
+        }
+        private void ReadFromFile()
+        {
+            TextInFile = Json.ReadFromFile<string>("json.json");
+
+        }
+
+    }
+}
